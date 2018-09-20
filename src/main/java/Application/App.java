dc2a2a50e85f86e474e5b3758aa18ef5f186b5fd
SebastianHalinski <sebastian.halinski@gmail.com>
2018-09-20 18:57:49 +0200

    Create base application starter

---
+++ src/main/java/Application/App.java
@@ -0,0 +1,13 @@
+package Application;
+
+public class App {
+
+    public static void main (String[] argv){
+        App app = new App();
+        app.startApp();
+    }
+
+    private void startApp() {
+
+    }
+}
